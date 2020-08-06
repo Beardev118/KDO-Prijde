bf0323dae450a46666f2f048ed829ff067b1d1a0
updev0612 <otoku118@aol.com>
2020-08-06 11:53:10 +1100

    NavigationBar adding RightButton(plus, dot-vertical)

--- App.js
+++ App.js
@@ -1,18 +1,108 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { Text, View } from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {IconButton, Colors} from 'react-native-paper';
+import {Icon} from 'react-native-material-ui';
 
 import Header from './src/component/Header';
 import Splash from './src/screen/Splash';
+const styles = StyleSheet.create({
+  headerRightContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+});
 
-const App = () => {
+function HomeScreen({navigation}) {
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: 'column'
-      }}>
-      <Splash />
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
-  )
+  );
 }
+
+function DetailsScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          // headerTitleStyle: {
+          //   fontWeight: 'bold',
+          // },
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: 'Kalendář',
+            headerRight: () => (
+              <View style={styles.headerRightContainer}>
+                <IconButton
+                  icon="plus-circle-outline"
+                  color={Colors.white}
+                  size={28}
+                  onPress={() => console.log('Pressed')}
+                />
+                <IconButton
+                  icon="dots-vertical"
+                  color={Colors.white}
+                  size={24}
+                  onPress={() => console.log('Pressed')}
+                />
+              </View>
+            ),
+          }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+// const App = () => {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         flexDirection: 'column',
+//       }}>
+//       <NavigationContainer>
+//         <Stack.Navigator>
+//           <Stack.Screen name="Home" component={HomeScreen} />
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//       {/* <Splash /> */}
+//     </View>
+//   );
+// };
 export default App;
--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {COLOR, ThemeContext, getTheme} from 'react-native-material-ui';
+import {Provider as PaperProvider} from 'react-native-paper';
 
-AppRegistry.registerComponent(appName, () => App);
+const uiTheme = {
+  palette: {
+    primaryColor: COLOR.green500,
+  },
+  toolbar: {
+    container: {
+      height: 50,
+    },
+  },
+};
+
+export default function Main() {
+  return (
+    <ThemeContext.Provider value={getTheme(uiTheme)}>
+      <App />
+    </ThemeContext.Provider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
